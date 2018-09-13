259ae4712af64e4ddba4d478c7b27854a56cecb4
dstrauss <fmdenver@gmail.com>
2018-09-13 00:11:14 -0400

    my timing troubleshooting

--- challenge1.js
+++ challenge1.js
@@ -7,19 +7,30 @@ var startButton = document.getElementById("start-button");
 var startCounting = function StartCounting(){
   //  alert("hello");
   var i = 0;
+/*   var sleep = function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+      if ((new Date().getTime() - start) > milliseconds){
+        break;
+      }
+    }
+  } */
 
  while(i<101){
- //  setInterval(function(){
     if(i % 3 != 0 && i % 5 != 0  ){
-        fizzBox.innerHTML += "<span class='text-warning'>" + i + "<br></span>";}
+        fizzBox.innerHTML += "<span class='text-warning'>" + i + "<br></span>";
+       }
     if(i%3 != 0 && i % 5 ===0){
-        fizzBox.innerHTML += "<span class='text-primary'> boom <br> </span>";}
+        fizzBox.innerHTML += "<span class='text-primary'> boom <br> </span>";
+        }
     if(i%3 === 0 && i % 5 != 0){
-        fizzBox.innerHTML += "<span class='text-success'> Fizz <br> </span>";}
+        fizzBox.innerHTML += "<span class='text-success'> Fizz <br> </span>";
+    }
     if(i%3 === 0 && i % 5 === 0 && i!=0){
-        fizzBox.innerHTML += "<span class='text-danger'> Fizz Boom Super Extreme!!!! <br> </span>";}
+        fizzBox.innerHTML += "<span class='text-danger'> Fizz Boom Super Extreme!!!! <br> </span>";
+        }
        i++;
-  //  },2000);
+     sleep(1000);
  }
 
 }
@@ -27,3 +38,4 @@ var startCounting = function StartCounting(){
 startButton.addEventListener("click",function(){
     startCounting();
 })
+
