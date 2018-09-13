eaf74cc9bf4206d0eff48500b8f352744adfcb4c
dstrauss <fmdenver@gmail.com>
2018-09-12 23:53:04 -0400

    updates

--- challenge1.js
+++ challenge1.js
@@ -1,21 +1,29 @@
-var fizzBox = document.getElementById("fizz-box"),
-    startButton = document.getElementById("start-button");
+"use strict";
 
+var fizzBox = document.getElementById("fizz-box");
+var startButton = document.getElementById("start-button");
+
+
+var startCounting = function StartCounting(){
+  //  alert("hello");
+  var i = 0;
+
+ while(i<101){
+ //  setInterval(function(){
+    if(i % 3 != 0 && i % 5 != 0  ){
+        fizzBox.innerHTML += "<span class='text-warning'>" + i + "<br></span>";}
+    if(i%3 != 0 && i % 5 ===0){
+        fizzBox.innerHTML += "<span class='text-primary'> boom <br> </span>";}
+    if(i%3 === 0 && i % 5 != 0){
+        fizzBox.innerHTML += "<span class='text-success'> Fizz <br> </span>";}
+    if(i%3 === 0 && i % 5 === 0 && i!=0){
+        fizzBox.innerHTML += "<span class='text-danger'> Fizz Boom Super Extreme!!!! <br> </span>";}
+       i++;
+  //  },2000);
+ }
+
+}
 
 startButton.addEventListener("click",function(){
     startCounting();
-});
-
-var startCounting = setTimeout(function StartCounting(){
-    alert("hello");
-    for(i=0;i<101;i++){
-    if (i % 3  != 0 && i % 5) {
-        fizzBox.innerHTML = i;
-    }
-    else{
-        return;
-    }
-};
-},100);
-
-
+})
